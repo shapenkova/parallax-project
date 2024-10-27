cf5f662538f69bace13d31c0a4e4b19024e77ba7
Anastasia Shapenkova <wondernasty@yandex.ru>
2024-10-27 16:08:50 +0500

    Создание сайта с Parallax эффектом при скролле

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
